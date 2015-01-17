de09a291e24975f6404dc918a438064268b58440
Marea <marea.cobb@email.wsu.edu>
2015-01-17 12:10:53 -0800

    Final project updates.

--- HW1.R
+++ HW1.R
@@ -2,7 +2,7 @@
 #' title: "BIOST 578 Homework 1"
 #' author: "Marea Cobb"
 #' date: "January 15, 2015"
-#' reviewer: 
+#' reviewer: ""
 #' ---
 
 
@@ -12,7 +12,7 @@ library(RColorBrewer)
 
 #' Creates the R markdown files.
 library(knitr)
-opts_knit$set(progress = FALSE, verbose = FALSE)
+opts_knit$set(progress = FALSE, verbose = FALSE, message=FALSE)
 spin(hair = "HW1.R", format = "Rmd")
 file.rename("HW1.md", "HW1.Rmd")
 
@@ -21,12 +21,15 @@ colors <- brewer.pal(9, "Set3")
 
 #' Loads the iris data
 data(iris)
+head(iris)
 
 #' Creates a new column for flower_id for each entry.
 iris$flower_id <- rownames(iris)
+head(iris)
 
 #' Uses the default melt method to rearrange the data for easier classification.
 iris_melted <- melt(iris)
+head(iris)
 
 #' Splits the variable at the "." to get the value name (length/width and flower type)
 split_variable <- strsplit(as.character(iris_melted$variable), split="\\.")
@@ -39,6 +42,7 @@ iris_melted$variable <- NULL
 
 #' Reshapes the data so iris$width and iris$length are now separte columns in the table 
 iris_cast <- dcast(iris_melted, formula=flower_id+Species+flower_part~measurement_type)
+head(iris_cast)
 
 #' Creates a plot displaying the length and width of petals and sepals classified by flower type.
 ggplot(data=iris_cast, colour=colors, aes(x=Width, y=Length, color=Species))+ # Add points and use free scales in the facet
